5f9beb76ca2439e53e106172f3454f5fdb506e8d
Chirag Mittal <chiragmittal36269@gmail.com>
2023-04-05 22:12:13 +0530

    First Commit

--- src/main/java/com/driver/services/TicketService.java
+++ src/main/java/com/driver/services/TicketService.java
@@ -93,9 +93,9 @@ public class TicketService {
 
         // object
         train.getBookedTickets().add(ticket);
-//        train.setNoOfSeats(train.getNoOfSeats() - numberOfSeatsWantToBook);
+        train.setNoOfSeats(train.getNoOfSeats() - numberOfSeatsWantToBook);
 
-//        trainRepository.save(train);
+        trainRepository.save(train);
         Ticket updatedTicket = ticketRepository.save(ticket);
 
         return updatedTicket.getTicketId();
